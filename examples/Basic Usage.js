38dcdafcdaf2472fca69f0536b57745ef3868aab
mark <mark1bean@gmail.com>
2026-04-01 12:43:55 +1100

    Renamed for clarity

--- examples/Basic Usage.js
+++ examples/Basic Usage.js
@@ -5,21 +5,17 @@ if ('undefined' === typeof Bez) {
 /**
  * Make a bez example.
  * @author m1b
- * @version 2022-12-23
+ * @version 2026-04-01
  * @requires Bez.js
  */
 (function () {
-
-    if (typeof Bez === 'undefined')
-        throw Error('Cannot find the required script file "Bez.js".');
 
     var doc = app.activeDocument;
 
     var bez = new Bez(
         {
-            paths: [
-                [[0, 0], [0, 12], [12, 12], [12, 0]]
-            ],
+            doc: doc,
+            paths: [[[0, 0], [0, 12], [12, 12], [12, 0]]],
             pathsClosed: [true],
         }
     );
